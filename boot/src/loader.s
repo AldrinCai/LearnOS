b83b291dc8a07a228c2c356ea3ea0f5bc98b397e
Aldrin <m13324538952_1@163.com>
2019-11-10 01:10:25 +0800

    fix(boot/include/boot.inc): fix bug in the code in the book
    DESC_LIMIT_VIDEO2 should be 0000_0000000000001011b, otherwise the segment base address of the memory will become 0x00008000 instead of the expected 0x000b8000
    
    ok

--- boot/src/loader.s
+++ boot/src/loader.s
@@ -5,16 +5,16 @@ section loader vstart=LOADER_BASE_ADDR
 
 	;构建 GDT 及内部的描述符
 	GDT_BASE: dd 0x00000000 
-				dd 0x00000000
+			dd 0x00000000
 
 	CODE_DESC: dd 0x0000FFFF 
-				dd DESC_CODE_HIGH4
+			dd DESC_CODE_HIGH4
 
 	DATA_STACK_DESC: dd 0x0000FFFF 
-						dd DESC_DATA_HIGH4
+					dd DESC_DATA_HIGH4
 
 	VIDEO_DESC: dd 0x80000007 
-					dd DESC_VIDEO_HIGH4
+				dd DESC_VIDEO_HIGH4
 
 	GDT_SIZE equ $ - GDT_BASE
 	GDT_LIMIT equ GDT_SIZE - 1
@@ -26,7 +26,7 @@ section loader vstart=LOADER_BASE_ADDR
 
 	;gdt 指针
 	gdt_ptr dw GDT_LIMIT 
-				dd GDT_BASE
+			dd GDT_BASE
 	
 	loadermsg db '2 loader in real.'
 
