310f5e4ccebc719ec57ad313a95f20f7444befbd
Aldrin <m13324538952_1@163.com>
2019-12-08 22:37:45 +0800

    fix(kernel/interrupt.c): fix make_idt_desc

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -12,17 +12,27 @@
 /*中断门描述符结构体*/
 struct gate_desc {
      uint16_t func_offset_low_word;
-     uint16_t selectot;
+     uint16_t selector;
      uint8_t dcount;
 
      uint8_t attribute;
      uint16_t func_offset_high_word;
-}; 
+};
 
 static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function);
 static struct gate_desc idt[IDT_DESC_CNT]; //中断描述符表
 
 extern intr_handler intr_entry_table[IDT_DESC_CNT]; //声明引用定义在 kernel.s 中的中断处理函数入口
+
+// 创建中断门描述符
+static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function){
+    p_gdesc->func_offset_low_word = ((uint32_t)function & 0xFFFF0000) >> 16;
+    p_gdesc->selector = SELECTOR_K_CODE;
+    p_gdesc->dcount = 0;
+    p_gdesc->attribute = attr;
+    p_gdesc->func_offset_high_word = ((uint32_t)function & 0xFFFF0000) >> 16;
+}
+
 
 /*初始化中断描述符表 */
 static void idt_desc_init(void){
